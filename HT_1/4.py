7a9e57683faa8b4ff4b43ee1cb2f1b6089304e51
Kateryna Smahlii <90623566+smagliy@users.noreply.github.com>
2021-11-17 17:46:55 +0200

    Added newfile.

--- HT_1/4.py
+++ HT_1/4.py
@@ -1,5 +1,8 @@
 # Write a script to concatenate N strings
-number = input('Number: ')
-word = 'abcdefg'
-print(list(word[:int(number)]))
-print(word[:int(number)])
+number = int(input('Number: '))
+list = []
+for a in range(number):
+    b = input("string :")
+    list.append(b)
+    a += 1
+print(''.join(list))
