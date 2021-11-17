bec5b72cced6558a882811c0fff622eb5b2fd343
Kateryna Smahlii <90623566+smagliy@users.noreply.github.com>
2021-11-17 17:02:10 +0200

    Added newfile.

--- HT_1/4.py
+++ HT_1/4.py
@@ -1,8 +1,5 @@
 # Write a script to concatenate N strings
-info = "We couldn't verify you mother's maiden name."
-
-first_name = input('First name: ')
-last_name = input('Last name: ')
-
-print('Hello'+', ' + first_name + ' ' + last_name + '!')
-print(info)
+number = input('Number: ')
+word = 'abcdefg'
+print(list(word[:int(number)]))
+print(word[:int(number)])
