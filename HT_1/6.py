d03afb9838e07b58c7e5127446d6b78ca3addc30
Kateryna Smahlii <90623566+smagliy@users.noreply.github.com>
2021-11-17 16:57:31 +0200

    Added newfile.

--- HT_1/6.py
+++ HT_1/6.py
@@ -1,10 +1,7 @@
 # Write a script to check whether a specified value is contained in a group of values
 list = [1, 5, 8, 3]
 number = int(input('Integer number: '))
-for i in list:
-    if i == number:
-        print(True)
-        break
-    else:
-        print(False)
-        break
+if number in list:
+    print(True)
+else:
+    print(False)
