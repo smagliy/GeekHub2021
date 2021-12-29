d146333bc5a2cb197367255fcdc73b84e94605cc
Kateryna Smahlii <90623566+smagliy@users.noreply.github.com>
2021-12-29 14:10:08 +0200

    Done

--- HT_12/1.py
+++ HT_12/1.py
@@ -10,18 +10,21 @@ import json
 
 db = sqlite3.connect('Pagination.db')
 cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS quote (author TEXT, texts_author TEXT, tags TEXT, page INT)")
+cur.execute("CREATE TABLE IF NOT EXISTS authors (author TEXT, date_birth TEXT, location_birth TEXT, details TEXT)")
+db.commit()
 
 
 def link_text():
     page_cons = 'http://quotes.toscrape.com'
     page = page_cons
-    list_all_text = list()
+    list_all_links = list()
     while True:
         try:
             page_response = requests.get(page)
             site = BeautifulSoup(page_response.text, 'lxml')
             element_list = site.select('.quote ')
-            list_elements_text = []
+            list_elements_link = []
             for i in element_list:
                 element_content = {
                     'author': i.select_one('span small.author').get_text(),
@@ -30,21 +33,30 @@ def link_text():
                     'tags': [elem.get_text() for elem in i.select('div.tags a.tag')],
                     'page': page,
                 }
-                list_elements_text.append(element_content)
-            list_all_text.append(list_elements_text)
+                fields = ['Author', 'Text', 'Tags', 'Page']
+                with open('quote_authors.csv', 'a', encoding='utf-8') as file:
+                    writer = csv.DictWriter(file, fieldnames=fields)
+                    writer.writerow({'Author': element_content['author'], 'Text': element_content['text_author'], 'Tags': element_content['tags'],
+                                     'Page': element_content['page']})
+                cur.execute("INSERT INTO quote (author, texts_author, tags, page) VALUES (?, ?, ?, ?)",
+                            (element_content['author'], element_content['text_author'], json.dumps(element_content['tags']), element_content['page']))
+                db.commit()
+                list_elements_link.append(element_content['link_author'])
+            list_all_links.append(list_elements_link)
             start = site.select_one('.pager .next a').get('href')
             page = page_cons + start
         except Exception:
             break
-    return list_all_text
+    return list_all_links
 
+link_text()
 
 def link_authors():
     list_all_authors = list()
     list_href_authors = list()
     for el in link_text():
         for el_1 in el:
-            href = el_1['link_author']
+            href = el_1
             list_href_authors.append(href)
     list_href_authors_new = list(set(list_href_authors))
     for link in list_href_authors_new:
@@ -59,42 +71,14 @@ def link_authors():
                 'place_birth': i.select_one('.author-born-location').get_text(),
                 'details': i.select_one('.author-description').get_text().replace("\n", '')
             }
-            list_elements_authors.append(element_author)
-        list_all_authors.append(list_elements_authors)
-    return list_all_authors
-
-
-def append_csv():
-    fields = ['Author', 'Text', 'Tags', 'Page']
-    with open('quote_authors.csv', 'a', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=fields)
-        #writer.writeheader()
-        for site in link_text():
-            for elem in site:
-                writer.writerow({'Author': elem['author'], 'Text': elem['text_author'], 'Tags': elem['tags'], 'Page': elem['page']})
-    fields_2 =['Author', 'Date birth', 'Location birth', 'Details']
-    with open('authors_info.csv', 'a', encoding='utf-8') as file:
-        write = csv.DictWriter(file, fieldnames=fields_2)
-        #write.writeheader()
-        for authors in link_authors():
-            for elem in authors:
-                write.writerow({'Author': elem['author'], 'Date birth': elem['date_birth'], 'Location birth': elem['place_birth'], 'Details': elem['details']})
-
-
-append_csv()
-
-def append_database():
-    cur.execute("CREATE TABLE IF NOT EXISTS quote (author TEXT, texts_author TEXT, tags TEXT, page INT)")
-    cur.execute("CREATE TABLE IF NOT EXISTS authors (author TEXT, date_birth TEXT, location_birth TEXT, details TEXT)")
-    db.commit()
-    for site in link_text():
-        for elem in site:
-            cur.execute("INSERT INTO quote (author, texts_author, tags, page) VALUES (?, ?, ?, ?)", (elem['author'], elem['text_author'], json.dumps(elem['tags']), elem['page']))
-            db.commit()
-    for link in link_authors():
-        for elem in link:
-            cur.execute("INSERT INTO authors (author, date_birth, location_birth, details) VALUES (?, ?, ?, ?)", (elem['author'], elem['date_birth'], elem['place_birth'], elem['details']))
+            fields_2 = ['Author', 'Date birth', 'Location birth', 'Details']
+            with open('authors_info.csv', 'a', encoding='utf-8') as file:
+                write = csv.DictWriter(file, fieldnames=fields_2)
+                write.writerow(
+                    {'Author': element_author['author'], 'Date birth': element_author['date_birth'], 'Location birth': element_author['place_birth'],
+                     'Details': element_author['details']})
+            cur.execute("INSERT INTO authors (author, date_birth, location_birth, details) VALUES (?, ?, ?, ?)",
+                        (element_author['author'], element_author['date_birth'], element_author['place_birth'], element_author['details']))
             db.commit()
 
-
-append_database()
+link_authors()
