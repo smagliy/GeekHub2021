c86f9284924fc4981638ce6eba30291147304331
smagliy <smaglijkata2002@gmail.com>
2022-01-17 23:02:09 +0200

    Scrapy project

--- HT_15/news/news/spiders/newstoscrape.py
+++ HT_15/news/news/spiders/newstoscrape.py
@@ -1,6 +1,7 @@
 import scrapy
 import csv
 import datetime
+
 from bs4 import BeautifulSoup
 
 
@@ -9,18 +10,20 @@ class NewstoscrapeSpider(scrapy.Spider):
     allowed_domains = ['vikka.ua']
     start_urls = ['http://vikka.ua/']
     data = input('Write date (2019/05/09): ')
+    fields = ['name', 'texts', 'tags', 'link']
+    with open(f'{data.replace("/", "_")}.csv', 'a+', encoding='utf-8') as f:
+        file = csv.DictWriter(f, fieldnames=fields)
+        file.writeheader()
 
     def start_requests(self):
         try:
             d = datetime.datetime.strptime(self.data, '%Y/%m/%d')
-            if datetime.datetime.now().day - d.day >= 0:
+            if (datetime.datetime.today()-d).days >= 0:
                 url_news = f'https://www.vikka.ua/{self.data}/'
                 yield scrapy.Request(
                         url=url_news,
                         callback=self.parse_news_page
                     )
-            else:
-                print('Data in future!')
         except Exception:
             print('Data isn`t correct!')
 
@@ -40,16 +43,18 @@ class NewstoscrapeSpider(scrapy.Spider):
         print(post)
 
     def parse_post(self, soup_post, link):
+        list_href = [el.get('href') for el in soup_post.select('link[rel="canonical"]')]
+        list_tags = ["#"+el.text for el in soup_post.select('a.post-tag')]
         dict_all = {
-            'link': link,
             'name': soup_post.select_one('h1').text,
-            'texts': [tex.text for tex in soup_post.select('div.entry-content p') if tex.text != ''],
-            'tags': ['#'+tag.text for tag in soup_post.select('a.post-tag')]
+            'texts': soup_post.select_one('div.entry-content.-margin-b').text,
+            'tags': " ".join(list_tags),
+            'link': list_href[0],
         }
-        fields = ['link', 'name', 'texts', 'tags']
+        fields = ['name', 'texts', 'tags', 'link']
         name = self.data.replace("/", "_")
         with open(f'{name}.csv', 'a+', encoding='utf-8') as f:
-            file = csv.DictWriter(f, fields)
+            file = csv.DictWriter(f, fieldnames=fields)
             file.writerow(dict_all)
         return dict_all
 
